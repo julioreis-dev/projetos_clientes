c348b6519c4528ff89e131cc1113409023dc9cfd
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-09-17 22:26:34 -0300

    Add files via upload

---
+++ readfile_new.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import xlrd
+
+
+def readarticle(files):
+    df = pd.read_csv(files, sep=',')
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d')
+    df['ACTIVE POWER'] = pd.to_datetime(df['ACTIVE POWER'])
+    df['ACTIVE POWER'] = df['ACTIVE POWER'].dt.strftime('%H:%M:%S')
+    df = df[['timestamp', 'ACTIVE POWER', 'COMS', 'STATUS']]
+    df.to_csv(files, index=False)
+
+
+def organizefiles(files):
+    df_data = pd.read_excel(files)
+    colunas = df_data.columns.values
+    return colunas
+
+
+def organizetupla(data):
+    listdata = []
+    lencollumn = len(data)
+    index1 = 1
+    index2 = 2
+    flag = True
+    while flag:
+        #print(column[index1], column[index2])
+        collumntupla = (column[index1], column[index2])
+        listdata.append(collumntupla)
+        index1 += 2
+        index2 += 2
+        if index2 > lencollumn:
+            flag = False
+    return listdata
+
+
+caminho = r'D:\OneDrive\Área de Trabalho\steag\Sao Pedro_2020_09_16.xlsx'
+#readarticle(caminho)
+column = organizefiles(caminho)
+x = organizetupla(column)
+print(x)
+for i in x:
+    df_fin = pd.read_excel(caminho)
+    df_fin = df_fin[[column[0], i[0], i[1]]]
+    print(df_fin)
