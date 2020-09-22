7d77160bc145330f9783a62708ab573e0f5723dc
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-09-21 21:01:51 -0300

    Add files via upload

--- readfileinverter.py
+++ readfileinverter.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import steagsupports
 from time import time
+import steagframes
 
 
 caminho = r'D:\OneDrive\Área de Trabalho\steag\atual\SPD - AGOSTO 2020_2020_09_18.xlsx'
@@ -13,18 +13,12 @@ steagsupports.sheetperiod(choose, destino, period)
 if choose != 0:
     print('Processando.......\n')
     v0 = time()
-    column = steagsupports.organizefiles(caminho)
-    lista_plant = steagsupports.organizetupla(column)
-    for i in lista_plant:
-        df_fin = pd.read_excel(caminho)
-        df_fin = df_fin.fillna(0.00)
-        df_fin = df_fin[[column[0], i[0], i[1]]]
-        df_fin.rename(columns={'Date': 'timestamp', i[0]: 'ACTIVE POWER', i[1]: 'COMS STATUS'}, inplace=True)
-        frame = steagsupports.readframe(df_fin)
-        filename = steagsupports.stamp(period, i[0], choose)
-        files = steagsupports.sheetdestination(destino, period, choose)
-        sheetname = f'{files}\{filename}.csv'
-        frame.to_csv(sheetname, index=False)
+    if choose == 1:
+        steagframes.calcinverter(caminho, period, choose, destino)
+    elif choose == 2:
+        steagframes.calcstrings(caminho, period, choose, destino)
+    elif choose == 3:
+        steagframes.calcwether(caminho, period, choose, destino)
     v1 = time()
     executiontime = (v1 - v0)
     print('Processo finalizado com sucesso!!!')
