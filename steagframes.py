3cafd7a47453a531194a582542def442661aac73
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-09-26 21:16:35 -0300

    Add files via upload

--- steagframes.py
+++ steagframes.py
@@ -3,12 +3,14 @@ import steagsupports
 
 
 # caminho-0, periodo - 1, choose - 2, destino - 3
-# Create dataframe to inverter equipment and save in sheet destiny
+# Function to create dataframe to inverter equipment and save in sheet destiny
 def calcinverter(*args):
     column = steagsupports.organizefiles(args[0])
     lista_plant = steagsupports.organizetuplainverter(column)
+    df = pd.read_excel(args[0])
     for i in lista_plant:
-        df_fin = pd.read_excel(args[0])
+        columnfilter = [column[0], column[1], i[0], i[1]]
+        df_fin = df.filter(items=columnfilter)
         df_fin = steagsupports.readframe(df_fin)
         df_fin = df_fin[[column[0], i[0], i[1]]]
         df_fin = df_fin.fillna(0.00)
@@ -17,40 +19,47 @@ def calcinverter(*args):
         files = steagsupports.sheetdestination(args[3], args[1], args[2])
         sheetname = r'{}\{}.csv'.format(files, namesfile)
         df_fin.to_csv(sheetname, index=False)
+        print('Arquivo {} salvo com sucesso!!!'.format(namesfile))
 
 
-# Create dataframe to stringbox equipment and save in sheet destiny
+# Function to create dataframe to stringbox equipment and save in sheet destiny
 def calcstringsbox(*args):
     column = steagsupports.organizefiles(args[0])
     lista_plant = steagsupports.organizetuplastringsbox(column)
+    df = pd.read_excel(args[0])
     for i in lista_plant:
-        df_fin = pd.read_excel(args[0])
+        columnfilter = [column[0], column[1], i[1], i[2], i[0]]
+        df_fin = df.filter(items=columnfilter)
         df_fin = steagsupports.readframe(df_fin)
-        df_fin = df_fin.fillna(0.00)
         df_fin = df_fin[[column[0], i[1], i[2], i[0]]]
+        df_fin = df_fin.fillna(0.00)
         df_fin.rename(columns={'Date': 'timestamp', i[1]: 'Current', i[2]: 'Power', i[0]: 'COMS STATUS'}, inplace=True)
         namesfile = steagsupports.stamp1(args[1], i[0], args[2], 'String box')
         files = steagsupports.sheetdestination(args[3], args[1], args[2])
         sheetname = r'{}\{}.csv'.format(files, namesfile)
         df_fin.to_csv(sheetname, index=False)
+        print('Arquivo {} salvo com sucesso!!!'.format(namesfile))
 
 
-# Create dataframe to string equipment and save in sheet destiny
+# Function to create dataframe to string equipment and save in sheet destiny
 def calcstrings(*args):
     column = steagsupports.organizefiles(args[0])
-    for i in column:
-        df_fin = pd.read_excel(args[0])
+    lista_plant = steagsupports.organizetuplastrings(column)
+    df = pd.read_excel(args[0])
+    for i in lista_plant:
+        columnfilter = [column[0], column[1], i[0]]
+        df_fin = df.filter(items=columnfilter)
         df_fin = steagsupports.readframe(df_fin)
         df_fin = df_fin.fillna(0.00)
         df_fin = df_fin[[column[0], i[0]]]
-        df_fin.rename(columns={'Date': 'timestamp', i[0]: 'Current'}, inplace=True)
+        df_fin.rename(columns={'Date': 'timestamp', i[0]: 'DC Current String'}, inplace=True)
         namesfile = steagsupports.stamp1(args[1], i[0], args[2], 'String')
         files = steagsupports.sheetdestination(args[3], args[1], args[2])
         sheetname = r'{}\{}.csv'.format(files, namesfile)
         df_fin.to_csv(sheetname, index=False)
 
 
-# Create dataframe to weather station equipment and save in sheet destiny
+# Function o create dataframe to weather station equipment and save in sheet destiny
 def calcweather(*args):
     plant = ['Sao Pedro', 'Juazeiro', 'Sol do Futuro']
     df_fin = pd.read_excel(args[0])
@@ -64,3 +73,67 @@ def calcweather(*args):
     # df_fin['timestamp'] = pd.to_datetime(df_fin['timestamp'])
     # df_fin['timestamp'] = df_fin['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
     print(df_fin)
+
+
+# Function to create dataframe to inverter equipment and save in sheet destiny
+# def calcinverter(*args):
+#     column = steagsupports.organizefiles(args[0])
+#     lista_plant = steagsupports.organizetuplainverter(column)
+#     for i in lista_plant:
+#         df_fin = pd.read_excel(args[0])
+#         df_fin = steagsupports.readframe(df_fin)
+#         df_fin = df_fin[[column[0], i[0], i[1]]]
+#         df_fin = df_fin.fillna(0.00)
+#         df_fin.rename(columns={'Date': 'timestamp', i[0]: 'ACTIVE POWER', i[1]: 'COMS STATUS'}, inplace=True)
+#         namesfile = steagsupports.stamp(args[1], i[0], args[2], 'Inverter')
+#         files = steagsupports.sheetdestination(args[3], args[1], args[2])
+#         sheetname = r'{}\{}.csv'.format(files, namesfile)
+#         df_fin.to_csv(sheetname, index=False)
+#         print('Arquivo {} salvo com sucesso!!!'.format(namesfile))
+
+
+# Function to create dataframe to stringbox equipment and save in sheet destiny
+# def calcstringsbox(*args):
+#     column = steagsupports.organizefiles(args[0])
+#     lista_plant = steagsupports.organizetuplastringsbox(column)
+#     for i in lista_plant:
+#         df_fin = pd.read_excel(args[0])
+#         df_fin = steagsupports.readframe(df_fin)
+#         df_fin = df_fin.fillna(0.00)
+#         df_fin = df_fin[[column[0], i[1], i[2], i[0]]]
+#         df_fin.rename(columns={'Date': 'timestamp', i[1]: 'Current', i[2]: 'Power', i[0]: 'COMS STATUS'}, inplace=True)
+#         namesfile = steagsupports.stamp1(args[1], i[0], args[2], 'String box')
+#         files = steagsupports.sheetdestination(args[3], args[1], args[2])
+#         sheetname = r'{}\{}.csv'.format(files, namesfile)
+#         df_fin.to_csv(sheetname, index=False)
+
+
+# Function to create dataframe to string equipment and save in sheet destiny
+# def calcstrings(*args):
+#     column = steagsupports.organizefiles(args[0])
+#     for i in column:
+#         df_fin = pd.read_excel(args[0])
+#         df_fin = steagsupports.readframe(df_fin)
+#         df_fin = df_fin.fillna(0.00)
+#         df_fin = df_fin[[column[0], i[0]]]
+#         df_fin.rename(columns={'Date': 'timestamp', i[0]: 'Current'}, inplace=True)
+#         namesfile = steagsupports.stamp1(args[1], i[0], args[2], 'String')
+#         files = steagsupports.sheetdestination(args[3], args[1], args[2])
+#         sheetname = r'{}\{}.csv'.format(files, namesfile)
+#         df_fin.to_csv(sheetname, index=False)
+
+
+# Function o create dataframe to weather station equipment and save in sheet destiny
+# def calcweather(*args):
+#     plant = ['Sao Pedro', 'Juazeiro', 'Sol do Futuro']
+#     df_fin = pd.read_excel(args[0])
+#     df_fin = steagsupports.readframe(df_fin)
+#     df_fin = df_fin.fillna(0.00)
+#     df_fin.rename(columns={'Date': 'timestamp'}, inplace=True)
+#     direct = steagsupports.sheetdestination(args[3], args[1], args[2])
+#     namestamp = f'{plant[args[2] - 1]}-Weather Station'
+#     sheetname = r'{}\{}.csv'.format(direct, namestamp)
+#     df_fin.to_csv(sheetname, index=False)
+#     # df_fin['timestamp'] = pd.to_datetime(df_fin['timestamp'])
+#     # df_fin['timestamp'] = df_fin['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
+#     print(df_fin)
