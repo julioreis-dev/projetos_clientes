7be9faebebad6524c98f4db2abdff84782301f29
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-09-27 23:12:58 -0300

    Add files via upload

--- steagframes.py
+++ steagframes.py
@@ -5,9 +5,9 @@ import steagsupports
 # caminho-0, periodo - 1, choose - 2, destino - 3
 # Function to create dataframe to inverter equipment and save in sheet destiny
 def calcinverter(*args):
-    column = steagsupports.organizefiles(args[0])
-    lista_plant = steagsupports.organizetuplainverter(column)
     df = pd.read_excel(args[0])
+    column = df.columns.values
+    lista_plant = steagsupports.organizetuplainverter(column)
     for i in lista_plant:
         columnfilter = [column[0], column[1], i[0], i[1]]
         df_fin = df.filter(items=columnfilter)
@@ -19,14 +19,14 @@ def calcinverter(*args):
         files = steagsupports.sheetdestination(args[3], args[1], args[2])
         sheetname = r'{}\{}.csv'.format(files, namesfile)
         df_fin.to_csv(sheetname, index=False)
-        print('Arquivo {} salvo com sucesso!!!'.format(namesfile))
+        print('Arquivo "{}" salvo com sucesso!!!'.format(namesfile))
 
 
 # Function to create dataframe to stringbox equipment and save in sheet destiny
 def calcstringsbox(*args):
-    column = steagsupports.organizefiles(args[0])
-    lista_plant = steagsupports.organizetuplastringsbox(column)
     df = pd.read_excel(args[0])
+    column = df.columns.values
+    lista_plant = steagsupports.organizetuplastringsbox(column)
     for i in lista_plant:
         columnfilter = [column[0], column[1], i[1], i[2], i[0]]
         df_fin = df.filter(items=columnfilter)
@@ -38,25 +38,25 @@ def calcstringsbox(*args):
         files = steagsupports.sheetdestination(args[3], args[1], args[2])
         sheetname = r'{}\{}.csv'.format(files, namesfile)
         df_fin.to_csv(sheetname, index=False)
-        print('Arquivo {} salvo com sucesso!!!'.format(namesfile))
+        print('Arquivo "{}" salvo com sucesso!!!'.format(namesfile))
 
 
 # Function to create dataframe to string equipment and save in sheet destiny
 def calcstrings(*args):
-    column = steagsupports.organizefiles(args[0])
-    lista_plant = steagsupports.organizetuplastrings(column)
     df = pd.read_excel(args[0])
-    for i in lista_plant:
-        columnfilter = [column[0], column[1], i[0]]
+    column = df.columns.values
+    for i in range(2, len(column)):
+        columnfilter = [column[0], column[1], column[i]]
         df_fin = df.filter(items=columnfilter)
         df_fin = steagsupports.readframe(df_fin)
+        df_fin = df_fin[[column[0], column[i]]]
         df_fin = df_fin.fillna(0.00)
-        df_fin = df_fin[[column[0], i[0]]]
-        df_fin.rename(columns={'Date': 'timestamp', i[0]: 'DC Current String'}, inplace=True)
-        namesfile = steagsupports.stamp1(args[1], i[0], args[2], 'String')
+        df_fin.rename(columns={'Date': 'timestamp', column[i]: 'DC Current String'}, inplace=True)
+        namesfile = steagsupports.stamp2(args[1], column[i], args[2], 'String')
         files = steagsupports.sheetdestination(args[3], args[1], args[2])
         sheetname = r'{}\{}.csv'.format(files, namesfile)
         df_fin.to_csv(sheetname, index=False)
+        print('Arquivo "{}" salvo com sucesso!!!'.format(namesfile))
 
 
 # Function o create dataframe to weather station equipment and save in sheet destiny
@@ -66,13 +66,12 @@ def calcweather(*args):
     df_fin = steagsupports.readframe(df_fin)
     df_fin = df_fin.fillna(0.00)
     df_fin.rename(columns={'Date': 'timestamp'}, inplace=True)
+    del df_fin['Time']
     direct = steagsupports.sheetdestination(args[3], args[1], args[2])
     namestamp = f'{plant[args[2] - 1]}-Weather Station'
     sheetname = r'{}\{}.csv'.format(direct, namestamp)
     df_fin.to_csv(sheetname, index=False)
-    # df_fin['timestamp'] = pd.to_datetime(df_fin['timestamp'])
-    # df_fin['timestamp'] = df_fin['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
-    print(df_fin)
+    print('Arquivo "{}" salvo com sucesso!!!'.format(namestamp))
 
 
 # Function to create dataframe to inverter equipment and save in sheet destiny
