25e3a54c51e3d6bf15fb959d0393d4d5775170d0
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-10-05 00:08:27 -0300

    Add files via upload

---
+++ v1/steag_weather.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import steagsupports
+
+
+# Classe to create dataframe to weather station equipment
+class Weatherstation:
+
+    @staticmethod
+    def calcweather(caminho, periodo, place, destino):
+        plant = ['Sao Pedro', 'Juazeiro', 'Sol do Futuro']
+        df_fin = pd.read_excel(caminho)
+        df_fin = Weatherstation.readframe(df_fin)
+        df_fin = df_fin.fillna(0.00)
+        df_fin.rename(columns={'Date': 'timestamp'}, inplace=True)
+        del df_fin['Time']
+        direct = steagsupports.sheetdestination(destino, periodo, place)
+        namestamp = f'{plant[place - 1]}-data-{periodo}-Weather station'
+        sheetname = r'{}\{}.csv'.format(direct, namestamp)
+        df_fin.to_csv(sheetname, index=False)
+        print('Arquivo "{}" salvo com sucesso!!!'.format(namestamp))
+        print('\nVolume de dados: 1 arquivo processado')
+
+    @staticmethod
+    def readframe(df):
+        df['Date'] = pd.to_datetime(df['Date'])
+        df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+        df['Time'] = pd.to_datetime(df['Time'])
+        df['Time'] = df['Time'].dt.strftime('%H:%M:%S')
+        df['Date'] = df['Date'] + ' ' + df['Time']
+        return df
