aca2820e0adb73b67fbe35501531f76354a92d6a
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-10-06 23:31:30 -0300

    Add files via upload

--- v1/steagsupports_factory.py
+++ v1/steagsupports_factory.py
@@ -36,35 +36,14 @@ def option1():
             time.sleep(3)
 
 
-# Option to choose equipments
-def option2():
-    flag = True
-    while flag:
-        opt2 = [0, 1, 2, 3, 4]
-        plant2 = int(input('################################################'
-                           '\nSelecione um tipo de equipamento disponível:'
-                           '\nDigite 1 --> Inversores'
-                           '\nDigite 2 --> Strings Box'
-                           '\nDigite 3 --> Strings'
-                           '\nDigite 4 --> Weather Station'
-                           '\nDigite 0 --> Sair.'
-                           '\n################################################'
-                           '\nPrezado usuário, escolha uma opção?'))
-        if plant2 in opt2:
-            return plant2
-        else:
-            print('Opção incorreta, tente novamente.')
-            time.sleep(3)
-
-
 def openfiles(*args):
     openfiles_list = []
     cont = 1
-    for n in range(0,4):
+    for n in range(0, 4):
         df_read = pd.read_excel(args[n])
         openfiles_list.append(df_read)
         print('Etapa {}/4 realizado com sucesso'.format(cont))
-        cont+=1
+        cont += 1
     time.sleep(3)
     return openfiles_list
 
@@ -84,13 +63,6 @@ def createsubsheets(pasta):
     for n in sheets:
         if not os.path.isdir(n):
             os.mkdir(n)
-
-
-# Function that return list with column names(Descarte)
-def organizefiles(files):
-    df_data = pd.read_excel(files)
-    colun = df_data.columns.values
-    return colun
 
 
 # Function to prepare date to use as names in sheets
@@ -141,115 +113,3 @@ def executiontime(*args):
         minute = resthr // 60
         seg = round((minute % 60) // 1, 2)
     return hr, minute, seg
-
-# Function to create name to each file inverter
-# def stamp(*args):
-#     if args[2] == 1:
-#         namestamp1 = f'Sao Pedro-data-{args[0]}-{args[3]}-{args[1][12:21]}'
-#         return namestamp1
-#     elif args[2] == 2:
-#         namestamp2 = f'Juazeiro-data-{args[0]}-{args[3]}-{args[1][11:22]}'
-#         return namestamp2
-#     elif args[2] == 3:
-#         namestamp3 = f'Sol do Futuro-data-{args[0]}-{args[3]}-{args[1][16:27]}'
-#         return namestamp3
-
-
-# Function to create name to each file stringbox
-# def stamp1(*args):
-#     if args[2] == 1:
-#         namestamp1 = f'Sao Pedro-data-{args[0]}-{args[3]}-{args[1][12:25]}'
-#         return namestamp1
-#     elif args[2] == 2:
-#         namestamp2 = f'Juazeiro-data-{args[0]}-{args[3]}-{args[1][11:25]}'
-#         return namestamp2
-#     elif args[2] == 3:
-#         namestamp3 = f'Sol do Futuro-data-{args[0]}-{args[3]}-{args[1][16:46]}'
-#         return namestamp3
-
-
-# Function to create name to each file string
-# def stamp2(*args):
-#     if args[2] == 1:
-#         namestamp1 = f'Sao Pedro-data-{args[0]}-{args[3]}-{args[1][12:27]}'
-#         return namestamp1
-#     elif args[2] == 2:
-#         namestamp2 = f'Juazeiro-data-{args[0]}-{args[3]}-{args[1][11:43]}'
-#         return namestamp2
-#     elif args[2] == 3:
-#         namestamp3 = f'Sol do Futuro-data-{args[0]}-{args[3]}-{args[1][16:52]}'
-#         return namestamp3
-
-
-# Function to create label name to each string
-# def labelstampstring(*args):
-#     if args[0] == 1:
-#         namestamp1 = f'{args[1][30:50]}'
-#         return namestamp1
-#     elif args[0] == 2:
-#         labelstamp2 = f'{args[1][46:66]}'
-#         return labelstamp2
-#     elif args[0] == 3:
-#         labelstamp3 = f'{args[1][55:75]}'
-#         return labelstamp3
-
-
-# Function to analize data of equipment strings (descarte)
-# def organizetuplastrings(listcol):
-#     flag = True
-#     listdata = []
-#     lencollumn = len(listcol)
-#     index1 = 2
-#     while flag:
-#         collumntupla = (listcol[index1])
-#         listdata.append(collumntupla)
-#         index1 += 1
-#         if index1 > lencollumn:
-#             flag = False
-#     return listdata
-
-
-# # Function to analize data of equipment inverter
-# def organizetuplainverter(listcol):
-#     flag = True
-#     listdata = []
-#     lencollumn = len(listcol)
-#     index1 = 2
-#     index2 = 3
-#     while flag:
-#         collumntupla = (listcol[index1], listcol[index2])
-#         listdata.append(collumntupla)
-#         index1 += 2
-#         index2 += 2
-#         if index2 > lencollumn:
-#             flag = False
-#     return listdata
-
-
-# Function to analize data of equipment stringbox
-# def organizetuplastringsbox(listcol):
-#     flag = True
-#     listdata = []
-#     lencollumn = len(listcol)
-#     index1 = 2
-#     index2 = 3
-#     index3 = 4
-#     while flag:
-#         collumntupla = (listcol[index1], listcol[index2], listcol[index3])
-#         listdata.append(collumntupla)
-#         index1 += 3
-#         index2 += 3
-#         index3 += 3
-#         if index2 > lencollumn:
-#             flag = False
-#     return listdata
-
-
-# Function to format date column and time column
-# def readframe(df):
-#     df['Date'] = pd.to_datetime(df['Date'])
-#     df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-#     df['Time'] = pd.to_datetime(df['Time'])
-#     df['Time'] = df['Time'].dt.strftime('%H:%M:%S')
-#     df['Date'] = df['Date'] + ' ' + df['Time']
-#     return df
